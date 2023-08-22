8802e827d1fe4a2eb8f05658cc0448123d8b9d8a
Taralas209 <alexej.zikunow@gmail.com>
2023-08-22 21:56:56 +0200

    Create model 'Flowers' and create file 'run_pooling'

--- telegram_bot/models.py
+++ telegram_bot/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+class Flower(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    image = models.ImageField(upload_to='flowers/')
+    OCCASIONS_CHOICES = [
+        ('birthday', 'День рождения'),
+        ('wedding', 'Свадьба'),
+        ('school', 'В школу'),
+        ('no_reason', 'Без повода'),
+        ('other', 'Другой повод'),
+    ]
+    occasion = models.CharField(max_length=20, choices=OCCASIONS_CHOICES, default='no_reason')
+
+    def __str__(self):
+        return self.name
