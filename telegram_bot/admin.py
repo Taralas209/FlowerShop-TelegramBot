e2a323b5169ff46c127ddb40f951863bf8a17ea7
Taralas209 <alexej.zikunow@gmail.com>
2023-08-26 20:19:02 +0200

    updated admi.py

--- telegram_bot/admin.py
+++ telegram_bot/admin.py
@@ -2,7 +2,15 @@ from django.contrib import admin
 from .models import Flower, Florist, Courier, Consultation, Order
 
 admin.site.register(Flower)
-admin.site.register(Florist)
-admin.site.register(Courier)
 admin.site.register(Consultation)
 admin.site.register(Order)
+
+
+@admin.register(Courier)
+class CourierAdmin(admin.ModelAdmin):
+    list_display = ['id', 'telegram_id', 'name']
+
+
+@admin.register(Florist)
+class FloristAdmin(admin.ModelAdmin):
+    list_display = ['id', 'telegram_id', 'name']
